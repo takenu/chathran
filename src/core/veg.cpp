55e9db527355baeeab6becc3cabe8c2d5b4ef01a
takenu <takenu@gmail.com>
2015-05-14 08:49:34 +0200

    Removed Selene source from Chathran repository
    I decided the current method of having a Selene folder is not elegant
    and not practical (especially now that Strata is starting to use Lua as
    well). Therefore I forked Selene and committed my earlier changes to
    that fork. I also used a FindSelene.cmake to find the include directory.
    
    From now on, Selene can be used by using '#include <selene.h>'. Note
    that the Selene fork to be used is at https://github.com/takenu/Selene.git.
    Using any other fork will not work (most importantly because of handling
    functions with float arguments).

--- src/core/veg.cpp
+++ src/core/veg.cpp
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <tiny/mesh/io/staticmesh.h>
 #include <tiny/img/io/image.h>
 
-#include "../tools/selene.h"
+#include <selene.h>
 
 #include "veg.hpp"
 
